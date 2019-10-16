d8c8f0d80cbab846f5d3fc7813ad109e97401c19
volodymyrmordas <vmordas@alteryx.com>
2019-10-17 01:16:09 +0300

    added mysql + init script

---
+++ db/run-mysql-test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "=============spin-up-mysql============"
+
+docker rm -f exmpl-mysql
+docker build . -t mysql:exmpl
+docker run -d -p 3306:3306 --name exmlp-mysql mysql:exmpl
+
