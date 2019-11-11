1329ee1263d78c017f125170dddd1fd3ea3a9aed
volodymyrmordas <vmordas@alteryx.com>
2019-11-11 16:03:15 +0200

    add joins task

---
+++ db/readme.md
@@ -0,0 +1,18 @@
+**miracle**
+
+**ENTRY** table - contains system entities
+
+**FILES** table - contains temporary information about remote datasource - filesystem, rdbms etc
+  
+  
+We have remote datasource. it contains folders and files. External application can connect to 
+the datasource and grab all information about inner structure.
+
+as a result files table will contains information like this:
+
+```
+/exmlp_1
+/exmlp_1/a.pdf
+/exmlp_1/b.pdf
+/exmlp_1/c.pdf
+```
--- db/sql/fixture.001.sql
+++ db/sql/fixture.001.sql
@@ -8,22 +8,22 @@ insert into exmlp.entry (id, path, type, version, created_by, hash_meta)
 values (uuid(), '/', 'D', 1, 'user-1', 'hash-1');
 
 insert into exmlp.entry (id, path, type, version, created_by, hash_meta)
-values (uuid(), '/metadata', 'D', 1, 'user-1', 'hash-1');
+values (uuid(), '/miracle', 'D', 1, 'user-1', 'hash-1');
 
 insert into exmlp.entry (id, path, type, version, created_by, hash_meta)
-values (uuid(), '/metadata/exmpl', 'D', 1, 'user-1', 'hash-1');
+values (uuid(), '/miracle/exmpl', 'D', 1, 'user-1', 'hash-1');
 
 insert into exmlp.entry (id, path, type, version, created_by, hash_meta)
-values (uuid(), '/metadata/exmpl/a.pdf', 'F', 1, 'user-1', 'hash-2');
+values (uuid(), '/miracle/exmpl/a.pdf', 'F', 1, 'user-1', 'hash-2');
 
 insert into exmlp.entry (id, path, type, version, created_by, hash_meta)
-values (uuid(), '/metadata/exmpl/b.pdf', 'F', 1, 'user-1', 'hash-2');
+values (uuid(), '/miracle/exmpl/b.pdf', 'F', 1, 'user-1', 'hash-2');
 
 insert into exmlp.entry (id, path, type, version, created_by, hash_meta)
-values (uuid(), '/metadata/exmpl/c.pdf', 'F', 1, 'user-1', 'hash-2');
+values (uuid(), '/miracle/exmpl/c.pdf', 'F', 1, 'user-1', 'hash-2');
 
 insert into exmlp.entry (id, path, type, version, created_by, hash_meta)
-values (uuid(), '/metadata/exmpl/d.pdf', 'F', 1, 'user-1', 'hash-2');
+values (uuid(), '/miracle/exmpl/d.pdf', 'F', 1, 'user-1', 'hash-2');
 
 
 insert into exmlp.files (sync_id, name, path, parent, type, extension, size, created, last_modified, access_time, url)
@@ -43,3 +43,9 @@ values ('sync-id-1', 'e.pdf', '/exmpl/e.pdf', null, 'F', 'pdf', '1000', '2019-10
 
 insert into exmlp.files (sync_id, name, path, parent, type, extension, size, created, last_modified, access_time, url)
 values ('sync-id-1', 'f.pdf', '/exmpl/f.pdf', null, 'F', 'pdf', '1000', '2019-10-17', '2019-10-18', null, null);
+
+insert into exmlp.files (sync_id, name, path, parent, type, extension, size, created, last_modified, access_time, url)
+values ('sync-id-1', 'datastore', '/exmpl/datastore', null, 'D', '', '', '2019-10-17', '2019-10-18', null, null);
+
+insert into exmlp.files (sync_id, name, path, parent, type, extension, size, created, last_modified, access_time, url)
+values ('sync-id-1', 'a.pdf', '/exmpl/datastore/a.pdf', null, 'F', 'pdf', '1000', '2019-10-17', '2019-10-18', null, null);
